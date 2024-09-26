5394df6b067f12f41a1642860061a5d202837155
HieuTranSE <khanghiu.contact@gmail.com>
2024-09-26 15:02:14 +0700

    [HieuTranSE] blogList v1

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,23 +1,26 @@
-import React, { Suspense, useEffect, forwardRef, Ref } from 'react';
-import { Box, Button, Input, Typography, FormLabel } from '@mui/joy';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, useGLTF, Text, Html } from '@react-three/drei';
-import { useSpring, a } from '@react-spring/three';
-import { Vector3 } from 'three';
+import React, { Suspense, useEffect, forwardRef } from "react";
+import { Box, Button, Input, Typography, FormLabel } from "@mui/joy";
+import { Grid } from "@mui/material";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF, Html } from "@react-three/drei";
+import BlogCard from "../../components/blogs/BlogCard";
+import "./index.css"; // Import the new CSS file
 
 const Model = forwardRef(({ path }: { path: string }, ref: any) => {
   const { scene } = useGLTF(path);
   const handlePointerUp = (e) => {
-    console.log(e)
+    console.log(e);
   };
   const animationRef = React.useRef<any>();
-  useFrame((state, delta)=>{
-      animationRef.current.rotation.y += delta/4;
-  })
+  useFrame((state, delta) => {
+    animationRef.current.rotation.y += delta / 4;
+  });
   return (
-    <mesh ref={animationRef}
+    <mesh
+      ref={animationRef}
       position={[0.5, 0, 2]}
-      onPointerUp={handlePointerUp}>
+      onPointerUp={handlePointerUp}
+    >
       <primitive object={scene} />
     </mesh>
   );
@@ -28,43 +31,24 @@ const Home: React.FC = (): JSX.Element => {
 
   useEffect(() => {
     if (meshRef.current) {
-      console.log('Mesh reference:', meshRef.current);
-      // You can access the mesh and perform any actions, like modifying its position or rotation
-      // meshRef.current.position.set(0, 1, 0);
+      console.log("Mesh reference:", meshRef.current);
     }
   });
-  return (
-    <Box sx={{ height: 720, backgroundColor: '#e6eced' }}>
-      <Canvas camera={{ position: [5, 2, 5], fov: 35, }} shadows>
-        {/* Ambient light for overall brightness */}
-        {/* <ambientLight intensity={0.7} /> */}
 
-        {/* Key directional light (mimicking sunlight) */}
+  return (
+    <Box className="model-container">
+      <Canvas camera={{ position: [5, 2, 5], fov: 35 }} shadows>
         <directionalLight
           position={[1, 2, 5]}
           intensity={4}
-          color={'0xff0033'}
+          color={"0xff0033"}
           castShadow
-
         />
-
-        {/* Spotlight to enhance focus on the model */}
-        {/* <spotLight
-          position={[10, 8, 6]}
-          angle={0.4}
-          intensity={1.5}
-          penumbra={1}
-          castShadow
-        /> */}
-
-        {/* A soft point light from the front */}
-        {/* <pointLight position={[0, 3, 5]} intensity={0.8} /> */}
-
         <Suspense fallback={null}>
-          <Model ref={meshRef} path='/glb/koifishbond.glb' />
+          <Model ref={meshRef} path="/glb/koifishbond.glb" />
         </Suspense>
         <Html position={[1, 2, 0]} distanceFactor={8}>
-          <Box sx={{ backgroundColor: 'white', padding: 2 }}>
+          <Box className="overlay-box">
             <Typography>Enter your information</Typography>
             <FormLabel>Your name</FormLabel>
             <Input placeholder="Type in here…" variant="soft" />
@@ -74,10 +58,71 @@ const Home: React.FC = (): JSX.Element => {
             <Input placeholder="Type in here…" variant="soft" />
           </Box>
         </Html>
-        <OrbitControls target={[0, 0, 0]} enableZoom={false} minDistance={5} maxDistance={10} enablePan={false} />
-        {/* <axesHelper></axesHelper> */}
+        <OrbitControls
+          target={[0, 0, 0]}
+          enableZoom={false}
+          minDistance={5}
+          maxDistance={10}
+          enablePan={false}
+        />
       </Canvas>
-      <Typography>Hello anh em</Typography>
+      <Typography className="header-content-blog"> Our Blogs </Typography>
+      <Typography className="under-content-blog">
+        Discover the harmony between Koi Fish and Feng Shui.
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        sx={{ mt: 4, padding: "30px", paddingLeft: "50px" }}
+      >
+        <Grid item xs={12} sm={6} md={4} sx={{ padding: "30px" }}>
+          <BlogCard
+            title="Color Meaning of Koi Fish in Feng Shui"
+            imageUrl="https://fengshuibeginner.com/wp-content/uploads/2022/10/Yin-Yang-Symbol-Meaning.jpg"
+            onClickUrl="/your-blog-url-1"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} sx={{ padding: "30px" }}>
+          <BlogCard
+            title="Symbolism of Koi Fish in Feng Shui"
+            imageUrl="https://fengshuibeginner.com/wp-content/uploads/2023/01/chinese-new-year-foods-nian-gao.jpg"
+            onClickUrl="/your-blog-url-2"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} sx={{ padding: "30px" }}>
+          <BlogCard
+            title="Feng Shui Koi Fish and Home Décor"
+            imageUrl="https://fengshuibeginner.com/wp-content/uploads/2019/07/feng-shui-centre-of-home.jpg"
+            onClickUrl="/your-blog-url-3"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} sx={{ padding: "30px" }}>
+          <BlogCard
+            title="Koi Fish in Feng Shui: Meaning & Benefits"
+            imageUrl="https://fengshuibeginner.com/wp-content/uploads/2019/01/Chinese-New-Year-Lanterns.jpg"
+            onClickUrl="/your-blog-url-3"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} sx={{ padding: "30px" }}>
+          <BlogCard
+            title="Seasonal Koi Pond Care Tips"
+            imageUrl="https://fengshuibeginner.com/wp-content/uploads/2019/04/koi-fish-feng-shui.jpg"
+            onClickUrl="/your-blog-url-3"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} sx={{ padding: "30px" }}>
+          <BlogCard
+            title="Koi Care for Good Feng Shui"
+            imageUrl="https://fengshuibeginner.com/wp-content/uploads/2022/07/koi-fish-feng-shui.jpg"
+            onClickUrl="/your-blog-url-3"
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
