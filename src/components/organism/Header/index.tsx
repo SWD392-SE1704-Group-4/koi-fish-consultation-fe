fc52be42b6667cd045629a168d947bbefb47d3ac
HieuTranSE <khanghiu.contact@gmail.com>
2024-10-03 15:51:25 +0700

    [HieuTran] update blog detail

--- src/components/organism/Header/index.tsx
+++ src/components/organism/Header/index.tsx
@@ -1,227 +1,270 @@
-import { useMediaQuery } from '@mui/material';
+import { useMediaQuery } from "@mui/material";
 import {
-    Stack, Typography, Button, Avatar, IconButton, Menu, MenuItem, ListItemDecorator,
-    Drawer, Box, MenuButton, Dropdown, Link
-} from '@mui/joy';
+  Stack,
+  Typography,
+  Button,
+  Avatar,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemDecorator,
+  Drawer,
+  Box,
+  MenuButton,
+  Dropdown,
+  Link,
+} from "@mui/joy";
 import {
-    Person as PersonIcon, Dashboard as DashboardIcon, Deck as DeckIcon,
-    ConfirmationNumber as ConfirmationNumberIcon, Logout as LogoutIcon, Menu as MenuIcon, ArrowDropDown
-} from '@mui/icons-material';
-import { RemoveAccessToken } from '../../../utils/tokens';
-import { useNavigate } from 'react-router-dom';
-import styles from './style.module.scss';
-import zIndex from '@mui/material/styles/zIndex';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { requestUserInfo } from '../../../features/auth/auth.actions';
-import { selectUserInfo, selectIsLoggedIn } from '../../../features/auth/auth.selectors';
-import { setIsLoggedIn } from '../../../features/auth/auth.reducers';
-import { setIsLoggedInAction } from '../../../features/auth';
+  Person as PersonIcon,
+  Dashboard as DashboardIcon,
+  Deck as DeckIcon,
+  ConfirmationNumber as ConfirmationNumberIcon,
+  Logout as LogoutIcon,
+  Menu as MenuIcon,
+  ArrowDropDown,
+} from "@mui/icons-material";
+import { RemoveAccessToken } from "../../../utils/tokens";
+import { useNavigate } from "react-router-dom";
+import styles from "./style.module.scss";
+import zIndex from "@mui/material/styles/zIndex";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { requestUserInfo } from "../../../features/auth/auth.actions";
+import {
+  selectUserInfo,
+  selectIsLoggedIn,
+} from "../../../features/auth/auth.selectors";
+import { setIsLoggedIn } from "../../../features/auth/auth.reducers";
+import { setIsLoggedInAction } from "../../../features/auth";
 
 const Header: React.FC<any> = (): JSX.Element => {
-    const userInfo = useSelector(selectUserInfo);
-    const isLoggedIn = useSelector(selectIsLoggedIn);
+  const userInfo = useSelector(selectUserInfo);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [drawerOpen, setDrawerOpen] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-    // Detect screen width less than 768px
-    const isMobile = useMediaQuery('(max-width:768px)');
+  // Detect screen width less than 768px
+  const isMobile = useMediaQuery("(max-width:768px)");
 
-    useEffect(() => {
-        dispatch(requestUserInfo());
-    }, []);
+  useEffect(() => {
+    dispatch(requestUserInfo());
+  }, []);
 
-    function logout() {
-        RemoveAccessToken();
-        dispatch(setIsLoggedInAction(false));
-        window.location.reload();
-    }
+  function logout() {
+    RemoveAccessToken();
+    dispatch(setIsLoggedInAction(false));
+    window.location.reload();
+  }
 
-    const toggleDrawer = () => {
-        setDrawerOpen(!drawerOpen);
-    };
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
-    return (
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        width: "100%",
+        height: "60px",
+        padding: {
+          xs: "10px",
+          md: "5px 80px",
+        },
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        boxSizing: "border-box",
+        borderBottom: "1px solid",
+      }}
+      className={styles.header}
+    >
+      <Typography sx={{ color: "black" }}>
+        <Link href="/home" sx={{ color: "black", textDecoration: "none" }}>
+          <img src={"/img/fengshui-logo.png"} width="100" height="60" />
+        </Link>
+      </Typography>
+
+      {isMobile ? (
+        // Mobile view: Show menu button
+        <IconButton onClick={toggleDrawer}>
+          <MenuIcon />
+        </IconButton>
+      ) : (
+        // Desktop view: Show links and user info
         <Stack
-            direction="row"
-            sx={{
-                width: '100%',
-                height: '60px',
-                padding: {
-                    xs: '10px',
-                    md: '5px 80px',
-                },
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                boxSizing: 'border-box',
-                borderBottom: '1px solid',
-            }}
-            className={styles.header}
+          direction="row"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 4,
+          }}
         >
-            <Typography
-                sx={{ color: 'black' }}
-            >
-                <img src={"/img/fengshui-logo.png"} width="100" height="60" />
-            </Typography>
-
-            {isMobile ? (
-                // Mobile view: Show menu button
-                <IconButton onClick={toggleDrawer}>
-                    <MenuIcon />
-                </IconButton>
-            ) : (
-                // Desktop view: Show links and user info
-                <Stack
-                    direction="row"
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        gap: 4
-                    }}
+          <Dropdown>
+            <MenuButton variant="plain" endDecorator={<ArrowDropDown />}>
+              <Typography sx={{ color: "black", fontWeight: "bold" }}>
+                Information
+              </Typography>
+            </MenuButton>
+            <Menu>
+              <MenuItem>
+                <Link
+                  href="/information/koi-fish"
+                  sx={{ color: "black", textDecoration: "none" }}
                 >
-                    <Dropdown>
-                        <MenuButton variant="plain" endDecorator={<ArrowDropDown />}>
-                            <Typography sx={{ color: 'black', fontWeight: 'bold' }}>Information</Typography>
+                  All Koi fish
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link
+                  href="/information/fish-pond"
+                  sx={{ color: "black", textDecoration: "none" }}
+                >
+                  All Fish pond
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link
+                  href="/information/fengshui-element"
+                  sx={{ color: "black", textDecoration: "none" }}
+                >
+                  Fengshui element
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Dropdown>
 
-                        </MenuButton>
-                        <Menu>
-                            <MenuItem>
-                                <Link href="/information/koi-fish" sx={{ color: 'black', textDecoration: 'none' }}>
-                                    All Koi fish
-                                </Link>
-                            </MenuItem>
-                            <MenuItem>
-                                <Link href="/information/fish-pond" sx={{ color: 'black', textDecoration: 'none' }}>
-                                    All Fish pond
-                                </Link>
-                            </MenuItem>
-                            <MenuItem>
-                                <Link href="/information/fengshui-element" sx={{ color: 'black', textDecoration: 'none' }}>
-                                    Fengshui element
-                                </Link>
-                            </MenuItem>
-                        </Menu>
-                    </Dropdown>
-
-                    <Link href="/annual-feng-shui" sx={{ color: 'black', textDecoration: 'none', fontWeight: 'bold' }}>
-                        Annual feng shui
-                    </Link>
-                    <Link href="/about-us" sx={{ color: 'black', textDecoration: 'none', fontWeight: 'bold' }}>
-                        About us
-                    </Link>
-                    {userInfo && isLoggedIn ? (
-                        <>
-                            <Dropdown>
-                                <MenuButton
-                                    slots={{ root: IconButton }}
-                                    slotProps={{ root: { variant: 'plain', color: 'neutral' } }}
-                                    sx={{ gap: 2 }}
-                                >
-                                    <Typography>{userInfo?.email}</Typography>
-                                    <Avatar
-                                        variant="outlined"
-                                        size="sm"
-                                        src={userInfo?.picture}
-                                    />
-                                </MenuButton>
-                                <Menu sx={{}}>
-                                    <MenuItem onClick={() => navigate('/me')}>
-                                        <ListItemDecorator><PersonIcon /></ListItemDecorator>
-                                        Dashboard
-                                    </MenuItem>
-                                    {userInfo?.role === 'admin' ? (
-                                        <MenuItem onClick={() => navigate('/me/dashboard')}>
-                                            <ListItemDecorator><DashboardIcon /></ListItemDecorator>
-                                            Dashboard
-                                        </MenuItem>
-                                    ) : (
-                                        <>
-                                            <MenuItem onClick={() => navigate('/me/profile')}>
-                                                <ListItemDecorator><DeckIcon /></ListItemDecorator>
-                                                My profile
-                                            </MenuItem>
-                                            <MenuItem onClick={() => navigate('/me/consultation')}>
-                                                <ListItemDecorator><ConfirmationNumberIcon /></ListItemDecorator>
-                                                My consultation
-                                            </MenuItem>
-                                        </>
-                                    )}
-                                    <MenuItem onClick={logout}>
-                                        <ListItemDecorator><LogoutIcon /></ListItemDecorator>
-                                        Logout
-                                    </MenuItem>
-                                </Menu>
-                            </Dropdown>
-                        </>
-                    ) : (
-                        <>
-                            <Button sx={{ backgroundColor: '#e6eced' }} onClick={() => navigate('/login')}>
-                                <Typography>Login</Typography>
-                            </Button>
-                            <Button sx={{ backgroundColor: '#e6eced' }} onClick={() => navigate('/register')}>
-                                <Typography>Register</Typography>
-                            </Button>
-                        </>
-                    )}
-                </Stack>
-            )}
-
-            {/* Drawer for mobile menu */}
-            <Drawer
-                anchor="right"
-                open={drawerOpen}
-                onClose={toggleDrawer}
-            >
-                <Box sx={{ width: 250 }}>
-                    <Stack direction="column" spacing={2} sx={{ padding: 2 }}>
-                        <Typography sx={{ color: 'black' }}>Koi fish</Typography>
-                        <Typography sx={{ color: 'black' }}>Fengshui bond</Typography>
-                        {userInfo ? (
-                            <>
-                                <Typography>{userInfo?.email}</Typography>
-                                <Button onClick={() => navigate('/me')}>
-                                    Dashboard
-                                </Button>
-                                {userInfo?.role === 'admin' ? (
-                                    <Button onClick={() => navigate('/me/dashboard')}>
-                                        Admin Dashboard
-                                    </Button>
-                                ) : (
-                                    <>
-                                        <Button onClick={() => navigate('/me/dashboard')}>
-                                            My timeshare
-                                        </Button>
-                                        <Button onClick={() => navigate('/me/dashboard')}>
-                                            My trips
-                                        </Button>
-                                    </>
-                                )}
-                                <Button onClick={logout}>
-                                    Logout
-                                </Button>
-                            </>
-                        ) : (
-                            <>
-                                <Button sx={{ backgroundColor: '#e6eced', color: 'black' }}>
-                                    <Link href="/login" disabled variant="plain">
-                                        Login
-                                    </Link>
-                                </Button>
-                                <Button sx={{ backgroundColor: '#e6eced', color: 'black' }}>
-                                    <Link href="/register" disabled variant="plain">
-                                        Register
-                                    </Link>
-                                </Button>
-                            </>
-                        )}
-                    </Stack>
-                </Box>
-            </Drawer>
+          <Link
+            href="/annual-feng-shui"
+            sx={{ color: "black", textDecoration: "none", fontWeight: "bold" }}
+          >
+            Annual feng shui
+          </Link>
+          <Link
+            href="/about-us"
+            sx={{ color: "black", textDecoration: "none", fontWeight: "bold" }}
+          >
+            About us
+          </Link>
+          {userInfo && isLoggedIn ? (
+            <>
+              <Dropdown>
+                <MenuButton
+                  slots={{ root: IconButton }}
+                  slotProps={{ root: { variant: "plain", color: "neutral" } }}
+                  sx={{ gap: 2 }}
+                >
+                  <Typography>{userInfo?.email}</Typography>
+                  <Avatar
+                    variant="outlined"
+                    size="sm"
+                    src={userInfo?.picture}
+                  />
+                </MenuButton>
+                <Menu sx={{}}>
+                  <MenuItem onClick={() => navigate("/me")}>
+                    <ListItemDecorator>
+                      <PersonIcon />
+                    </ListItemDecorator>
+                    Dashboard
+                  </MenuItem>
+                  {userInfo?.role === "admin" ? (
+                    <MenuItem onClick={() => navigate("/me/dashboard")}>
+                      <ListItemDecorator>
+                        <DashboardIcon />
+                      </ListItemDecorator>
+                      Dashboard
+                    </MenuItem>
+                  ) : (
+                    <>
+                      <MenuItem onClick={() => navigate("/me/profile")}>
+                        <ListItemDecorator>
+                          <DeckIcon />
+                        </ListItemDecorator>
+                        My profile
+                      </MenuItem>
+                      <MenuItem onClick={() => navigate("/me/consultation")}>
+                        <ListItemDecorator>
+                          <ConfirmationNumberIcon />
+                        </ListItemDecorator>
+                        My consultation
+                      </MenuItem>
+                    </>
+                  )}
+                  <MenuItem onClick={logout}>
+                    <ListItemDecorator>
+                      <LogoutIcon />
+                    </ListItemDecorator>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </Dropdown>
+            </>
+          ) : (
+            <>
+              <Button
+                sx={{ backgroundColor: "#e6eced" }}
+                onClick={() => navigate("/login")}
+              >
+                <Typography>Login</Typography>
+              </Button>
+              <Button
+                sx={{ backgroundColor: "#e6eced" }}
+                onClick={() => navigate("/register")}
+              >
+                <Typography>Register</Typography>
+              </Button>
+            </>
+          )}
         </Stack>
-    );
+      )}
+
+      {/* Drawer for mobile menu */}
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
+        <Box sx={{ width: 250 }}>
+          <Stack direction="column" spacing={2} sx={{ padding: 2 }}>
+            <Typography sx={{ color: "black" }}>Koi fish</Typography>
+            <Typography sx={{ color: "black" }}>Fengshui bond</Typography>
+            {userInfo ? (
+              <>
+                <Typography>{userInfo?.email}</Typography>
+                <Button onClick={() => navigate("/me")}>Dashboard</Button>
+                {userInfo?.role === "admin" ? (
+                  <Button onClick={() => navigate("/me/dashboard")}>
+                    Admin Dashboard
+                  </Button>
+                ) : (
+                  <>
+                    <Button onClick={() => navigate("/me/dashboard")}>
+                      My timeshare
+                    </Button>
+                    <Button onClick={() => navigate("/me/dashboard")}>
+                      My trips
+                    </Button>
+                  </>
+                )}
+                <Button onClick={logout}>Logout</Button>
+              </>
+            ) : (
+              <>
+                <Button sx={{ backgroundColor: "#e6eced", color: "black" }}>
+                  <Link href="/login" disabled variant="plain">
+                    Login
+                  </Link>
+                </Button>
+                <Button sx={{ backgroundColor: "#e6eced", color: "black" }}>
+                  <Link href="/register" disabled variant="plain">
+                    Register
+                  </Link>
+                </Button>
+              </>
+            )}
+          </Stack>
+        </Box>
+      </Drawer>
+    </Stack>
+  );
 };
 
 export default Header;
